fb38687d441be64dfb37beffe9aa216edfa3aa67
Aurimas J <aurimas.janulaitis@teltonika.lt>
2023-10-31 08:38:18 +0200

    Update error_handling.lua

--- error_handling.lua
+++ error_handling.lua
@@ -1,15 +1,15 @@
-local error_handling = {}
+local ErrorHandling = {}
 
-function error_handling.handle_error(err)
+function ErrorHandling.handle_error(err)
     return err
 end
 
-function error_handling.custom_error(err_message)
-    return error_handling.handle_error(err_message)
+function ErrorHandling.custom_error(err_message)
+    return ErrorHandling.handle_error(err_message)
 end
 
-function error_handling.handle_error_message(msg)
+function ErrorHandling.handle_error_message(msg)
     return string.match(msg, "%b[]%s(.+)")
 end
 
-return error_handling
+return ErrorHandling
